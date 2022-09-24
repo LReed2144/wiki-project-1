ecd0f3df59a42cd4ef6b2366e537a14afe234e98
Lyndsey Reed <43240450+LReed2144@users.noreply.github.com>
2022-09-23 21:57:22 -0700

    markdown to html. create pages

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 # URL configuration is defined
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<str:title>", views.entry, name="entry")
+
 ]
