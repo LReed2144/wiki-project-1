1d51c743e14041f3d17b8e5740d756ca3e8461cf
Lyndsey Reed <43240450+LReed2144@users.noreply.github.com>
2022-09-23 23:36:34 -0700

    search page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,6 +5,6 @@ from . import views
 # URL configuration is defined
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.entry, name="entry")
-
+    path("<str:title>", views.entry, name="entry"),
+    path("search/", views.search, name="search")
 ]
